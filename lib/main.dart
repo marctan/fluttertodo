ae68a2f3c068f19b2e18cadc76528661e243f692
Marc Tan <marcqtan@gmail.com>
2022-02-03 09:13:41 +0800

    Transfer home screen into its own folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertodo/screens/tabs/all_screen.dart';
-import 'package:fluttertodo/screens/tabs/complete_screen.dart';
-import 'package:fluttertodo/screens/tabs/incomplete_screen.dart';
+import 'package:fluttertodo/screens/main/home_screen.dart';
 import 'package:fluttertodo/util/constants.dart';
 
 void main() {
@@ -22,68 +20,6 @@ class MyApp extends StatelessWidget {
         ),
         fontFamily: 'Montserrat',
       ),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  int _selectedIndex = 0;
-
-  final _bottomBody = [
-    const AllScreen(),
-    const CompleteScreen(),
-    const IncompleteScreen()
-  ];
-
-  _itemBarTapped(index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Todo App',
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        backgroundColor: Theme.of(context).primaryColor,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        onTap: _itemBarTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.today_outlined,
-            ),
-            label: 'All',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.check_box_outlined,
-            ),
-            label: 'Complete',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.disabled_by_default_outlined,
-            ),
-            label: 'Incomplete',
-          )
-        ],
-      ),
-      body: _bottomBody[_selectedIndex],
     );
   }
 }
